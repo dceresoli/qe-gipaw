0a618414083977894825386524cf08077cec99d4
dceresoli <dceresoli@1bc762a1-58a0-405b-a543-1f07c6ff0251>
2010-04-16 02:11:40 +0000

    cgsolve_all has been synchronized with the one in PH. (D.C.)

--- GIPAW/cgsolve_all.f90
+++ GIPAW/cgsolve_all.f90
@@ -8,7 +8,7 @@
 !
 !----------------------------------------------------------------------
 subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
-     ndmx, ndim, ethr, ik, kter, conv_root, anorm, nbnd)
+     ndmx, ndim, ethr, ik, kter, conv_root, anorm, nbnd, npol)
   !----------------------------------------------------------------------
   !
   !     iterative solution of the linear system:
@@ -55,9 +55,10 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
   !   revised (extensively)       6 Apr 1997 by A. Dal Corso & F. Mauri
   !   revised (to reduce memory) 29 May 2004 by S. de Gironcoli
   !
-  USE kinds,     only : DP
-  USE mp_global, only : intra_pool_comm
-  USE mp,        only : mp_sum
+  USE kinds, only : DP
+  USE mp_global, ONLY: intra_pool_comm
+  USE mp,        ONLY: mp_sum
+
   implicit none
   !
   !   first the I/O variables
@@ -66,21 +67,22 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
              ndim, & ! input: the actual dimension of the vectors
              kter, & ! output: counter on iterations
              nbnd, & ! input: the number of bands
+             npol, & ! input: number of components of the wavefunctions
              ik      ! input: the k point
 
   real(DP) :: &
              e(nbnd), & ! input: the actual eigenvalue
              anorm,   & ! output: the norm of the error in the solution
-             h_diag(ndmx,nbnd), & ! input: an estimate of ( H - \epsilon )
+             h_diag(ndmx*npol,nbnd), & ! input: an estimate of ( H - \epsilon )
              ethr       ! input: the required precision
 
   complex(DP) :: &
-             dpsi (ndmx, nbnd), & ! output: the solution of the linear syst
-             d0psi (ndmx, nbnd)   ! input: the known term
+             dpsi (ndmx*npol, nbnd), & ! output: the solution of the linear syst
+             d0psi (ndmx*npol, nbnd)   ! input: the known term
 
   logical :: conv_root ! output: if true the root is converged
-  external h_psi, &    ! input: the routine computing h_psi
-           cg_psi      ! input: the routine computing cg_psi
+  external h_psi       ! input: the routine computing h_psi
+  external cg_psi      ! input: the routine computing cg_psi
   !
   !  here the local variables
   !
@@ -97,9 +99,10 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
   !  the delta gradient
   !  the conjugate gradient
   !  work space
-  complex(DP) ::  dcgamma, dclambda, zdotc
+  complex(DP) ::  dcgamma, dclambda
   !  the ratio between rho
   !  step length
+  complex(DP), external :: zdotc 
   !  the scalar product
   real(DP), allocatable :: rho (:), rhoold (:), eu (:), a(:), c(:)
   ! the residue
@@ -109,7 +112,8 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
   ! coefficient of quadratic form
   !
   call start_clock ('cgsolve')
-  allocate ( g(ndmx,nbnd), t(ndmx,nbnd), h(ndmx,nbnd), hold(ndmx ,nbnd) )    
+  allocate ( g(ndmx*npol,nbnd), t(ndmx*npol,nbnd), h(ndmx*npol,nbnd), &
+             hold(ndmx*npol ,nbnd) )    
   allocate (a(nbnd), c(nbnd))    
   allocate (conv ( nbnd))    
   allocate (rho(nbnd),rhoold(nbnd))    
@@ -120,6 +124,10 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
   do ibnd = 1, nbnd
      conv (ibnd) = 0
   enddo
+  g=(0.d0,0.d0)
+  t=(0.d0,0.d0)
+  h=(0.d0,0.d0)
+  hold=(0.d0,0.d0)
   do iter = 1, maxter
      !
      !    compute the gradient. can reuse information from previous step
@@ -129,6 +137,12 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
         do ibnd = 1, nbnd
            call zaxpy (ndim, (-1.d0,0.d0), d0psi(1,ibnd), 1, g(1,ibnd), 1)
         enddo
+        IF (npol==2) THEN
+           do ibnd = 1, nbnd
+              call zaxpy (ndim, (-1.d0,0.d0), d0psi(ndmx+1,ibnd), 1, &
+                                              g(ndmx+1,ibnd), 1)
+           enddo
+        END IF
      endif
      !
      !    compute preconditioned residual vector and convergence check
@@ -137,20 +151,21 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
      do ibnd = 1, nbnd
         if (conv (ibnd) .eq.0) then
            lbnd = lbnd+1
-           call zcopy (ndim, g (1, ibnd), 1, h (1, ibnd), 1)
+           call zcopy (ndmx*npol, g (1, ibnd), 1, h (1, ibnd), 1)
            call cg_psi(ndmx, ndim, 1, h(1,ibnd), h_diag(1,ibnd) )
-           rho(lbnd) = zdotc (ndim, h(1,ibnd), 1, g(1,ibnd), 1)
+           rho(lbnd) = zdotc (ndmx*npol, h(1,ibnd), 1, g(1,ibnd), 1)
         endif
      enddo
      kter_eff = kter_eff + DBLE (lbnd) / DBLE (nbnd)
 #ifdef __PARA
-     call mp_sum(  rho , intra_pool_comm )
+     call mp_sum(  rho(1:lbnd) , intra_pool_comm )
 #endif
      do ibnd = nbnd, 1, -1
         if (conv(ibnd).eq.0) then
            rho(ibnd)=rho(lbnd)
            lbnd = lbnd -1
            anorm = sqrt (rho (ibnd) )
+!           write(6,*) ibnd, anorm
            if (anorm.lt.ethr) conv (ibnd) = 1
         endif
      enddo
@@ -169,10 +184,10 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
 !
 !          change sign to h 
 !
-           call dscal (2 * ndim, - 1.d0, h (1, ibnd), 1)
+           call dscal (2 * ndmx * npol, - 1.d0, h (1, ibnd), 1)
            if (iter.ne.1) then
               dcgamma = rho (ibnd) / rhoold (ibnd)
-              call zaxpy (ndim, dcgamma, hold (1, ibnd), 1, h (1, ibnd), 1)
+              call zaxpy (ndmx*npol, dcgamma, hold (1, ibnd), 1, h (1, ibnd), 1)
            endif
 
 !
@@ -180,7 +195,7 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
 ! it is later set to the current (becoming old) value of h 
 !
            lbnd = lbnd+1
-           call zcopy (ndim, h (1, ibnd), 1, hold (1, lbnd), 1)
+           call zcopy (ndmx*npol, h (1, ibnd), 1, hold (1, lbnd), 1)
            eu (lbnd) = e (ibnd)
         endif
      enddo
@@ -195,13 +210,13 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
      do ibnd = 1, nbnd
         if (conv (ibnd) .eq.0) then
            lbnd=lbnd+1
-           a(lbnd) = zdotc (ndim, h(1,ibnd), 1, g(1,ibnd), 1)
-           c(lbnd) = zdotc (ndim, h(1,ibnd), 1, t(1,lbnd), 1)
+           a(lbnd) = zdotc (ndmx*npol, h(1,ibnd), 1, g(1,ibnd), 1)
+           c(lbnd) = zdotc (ndmx*npol, h(1,ibnd), 1, t(1,lbnd), 1)
         end if
      end do
 #ifdef __PARA
-     call mp_sum(  a, intra_pool_comm )
-     call mp_sum(  c, intra_pool_comm )
+     call mp_sum(  a(1:lbnd), intra_pool_comm )
+     call mp_sum(  c(1:lbnd), intra_pool_comm )
 #endif
      lbnd=0
      do ibnd = 1, nbnd
@@ -211,16 +226,16 @@ subroutine cgsolve_all (h_psi, cg_psi, e, d0psi, dpsi, h_diag, &
            !
            !    move to new position
            !
-           call zaxpy (ndim, dclambda, h(1,ibnd), 1, dpsi(1,ibnd), 1)
+           call zaxpy (ndmx*npol, dclambda, h(1,ibnd), 1, dpsi(1,ibnd), 1)
            !
            !    update to get the gradient
            !
            !g=g+lam
-           call zaxpy (ndim, dclambda, t(1,lbnd), 1, g(1,ibnd), 1)
+           call zaxpy (ndmx*npol, dclambda, t(1,lbnd), 1, g(1,ibnd), 1)
            !
            !    save current (now old) h and rho for later use
            ! 
-           call zcopy (ndim, h(1,ibnd), 1, hold(1,ibnd), 1)
+           call zcopy (ndmx*npol, h(1,ibnd), 1, hold(1,ibnd), 1)
            rhoold (ibnd) = rho (ibnd)
         endif
      enddo
