96dbcb545f8e5164b1e90fde9a76d3311dd4b89e
giannozzi <giannozzi@1bc762a1-58a0-405b-a543-1f07c6ff0251>
2012-02-21 13:08:40 +0000

    Call to "divide" updated

--- src/init_gipaw_1.f90
+++ src/init_gipaw_1.f90
@@ -288,7 +288,7 @@ subroutine init_gipaw_1
   !     fill the interpolation table tab
   !
   pref = fpi / sqrt ( omega )
-  call divide ( nqx, startq, lastq )
+  call divide ( intra_pool_comm, nqx, startq, lastq )
   do nt = 1, ntyp
      paw_recon(nt)%paw_tab (:,:) = 0.0_dp
      
