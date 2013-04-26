1ccefaec12d3a79b95cca2080b2ac9ad385b0600
giannozz <giannozz@1bc762a1-58a0-405b-a543-1f07c6ff0251>
2013-04-26 15:17:36 +0000

    Aligned with last commit: orthonormalization of atomic wavefunctions is done by a separate routine

--- src/orthoatwfc1.f90
+++ src/orthoatwfc1.f90
@@ -22,8 +22,6 @@ SUBROUTINE orthoatwfc1(ik)
   USE uspp,             ONLY : nkb, vkb
   USE becmod,           ONLY : allocate_bec_type, deallocate_bec_type, &
                                bec_type, becp, calbec
-  USE mp_global,        ONLY : intra_pool_comm
-  USE mp,               ONLY : mp_sum
   USE control_flags,    ONLY : gamma_only
   USE noncollin_module, ONLY : noncolin, npol
   USE io_global,        ONLY : stdout
@@ -32,10 +30,8 @@ SUBROUTINE orthoatwfc1(ik)
   IMPLICIT NONE
   integer, intent(in) :: ik
   !-- local variables ----------------------------------------------------
-  complex(dp), allocatable :: wfcatom(:,:), work(:,:), overlap(:,:)
-  real(dp), allocatable :: e(:)
-  complex :: temp
-  integer :: i, j, k
+  complex(dp), allocatable :: wfcatom(:,:)
+  LOGICAL :: normalize_only
 
   allocate(wfcatom(npwx,natomwfc))    
 
@@ -49,57 +45,14 @@ SUBROUTINE orthoatwfc1(ik)
   if (U_projection=="atomic") goto 200
 
   ! orthogonalize
-  allocate(overlap(natomwfc,natomwfc), work(natomwfc,natomwfc))    
-  allocate(e(natomwfc))    
-  overlap(:,:) = (0.d0,0.d0)
-  work(:,:) = (0.d0,0.d0)
 
-  ! calculate overlap
-  call zgemm('c', 'n', natomwfc, natomwfc, npw, (1.d0,0.d0), &
-             wfcatom, npwx, swfcatom, npwx, (0.d0, 0.d0), overlap, natomwfc)
-#ifdef __MPI
-  call mp_sum(overlap, intra_pool_comm)
-#endif
-
-  if (U_projection == "norm-atomic") then
-    do i = 1, natomwfc
-      do j = i+1, natomwfc
-        overlap(i,j) = (0.d0,0.d0)
-        overlap(j,i) = (0.d0,0.d0)
-      enddo
-    enddo
-  endif
-
-  ! find O^-.5
-  call cdiagh(natomwfc, overlap, natomwfc, e, work)
-  do i = 1, natomwfc
-     e(i) = 1.d0/sqrt(e(i))
-  enddo
-
-  do i = 1, natomwfc
-    do j = i, natomwfc
-      temp = (0.d0, 0.d0)
-      do k = 1, natomwfc
-         temp = temp + e(k) * work(j,k) * conjg(work(i,k))
-      enddo
-      overlap(i,j) = temp
-      if (i /= j) overlap(j,i) = conjg(temp)
-     enddo
-  enddo
-
-  ! trasform atomic orbitals O^-.5 psi
-  do i = 1, npw
-    work(:,1) = (0.d0,0.d0)
-    call zgemv('n', natomwfc, natomwfc, (1.d0,0.d0), overlap, &
-                natomwfc, swfcatom (i,1), npwx, (0.d0,0.d0), work, 1)
-    call zcopy(natomwfc, work, 1, swfcatom (i,1), npwx)
-  enddo
-  deallocate(overlap, work, e)
-        
+  normalize_only = (U_projection == "norm-atomic") 
+  CALL ortho_swfc ( normalize_only, natomwfc, wfcatom, swfcatom )
+  !
 200 continue
-  !!!
+  !
   CALL copy_U_wfc ()
-  !!!
-  deallocate(overlap, work, e, wfcatom)
+  !
+  deallocate(wfcatom)
 
 END SUBROUTINE orthoatwfc1
