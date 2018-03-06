76a91003aec4d3a6fcdde4bd902c29be0993c98b
Davide Ceresoli <dceresoli@gmail.com>
2018-03-06 14:04:41 +0100

    EFG tests done!

---
+++ tests/EFG/NC/run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pw=$HOME/Codes/q-e/bin/pw.x
+gipaw=../../../bin/gipaw.x
+
+set -x
+mpirun -np 1 $pw <quartz-scf.in >quartz-scf.out_1cpu
+mpirun -np 1 $gipaw <quartz-efg.in >quartz-efg.out_1cpu
+
+mpirun -np 4 $pw <quartz-scf.in >quartz-scf.out_4cpu
+mpirun -np 4 $gipaw <quartz-efg.in >quartz-efg.out_4cpu
+
+mpirun -np 4 $pw -npool 2 <quartz-scf.in >quartz-scf.out_4cpu_2pool
+mpirun -np 4 $gipaw -npool 2 <quartz-efg.in >quartz-efg.out_4cpu_2pool
+
+rm -f *.magres
+grep ! *scf.out* > RESULTS.txt
+grep "Cq" *efg.out* >>RESULTS.txt
