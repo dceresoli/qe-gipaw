4cd5537c0dc5f88195b0a20bca5bdb2afee7bbe8
Davide Ceresoli <dceresoli@gmail.com>
2018-03-08 15:17:22 +0100

    DFT+U NMR test on CeO2

---
+++ tests/Hubbard/run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pw=$HOME/Codes/q-e/bin/pw.x
+gipaw=../../../bin/gipaw.x
+
+set -x
+mpirun -np 4 $pw <CeO2-scf-U0.in >CeO2-scf-U0.out
+mpirun -np 4 $gipaw <CeO2-nmr.in >CeO2-nmr-U0.out
+
+mpirun -np 4 $pw <CeO2-scf-U5.in >CeO2-scf-U5.out
+mpirun -np 4 $gipaw <CeO2-nmr.in >CeO2-nmr-U5.out
+
+rm -f *.magres
+grep ! *scf.out* > RESULTS.txt
+grep "Total sigma:" *nmr.out* >>RESULTS.txt
