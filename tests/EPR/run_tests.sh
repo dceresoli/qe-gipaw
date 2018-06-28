b5621f8c96a99e314a8c309fe4bc87d0d3956550
Davide Ceresoli <dceresoli@gmail.com>
2018-06-28 15:47:00 +0200

    towards v6.3: EPR tests

---
+++ tests/EPR/run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pw=$HOME/Codes/q-e/bin/pw.x
+gipaw=../../bin/gipaw.x
+
+set -x
+mpirun -np 4 $pw <KCl_O2-scf.in >KCl_O2-scf.out_4cpu
+mpirun -np 4 $gipaw <KCl_O2-gtensor.in >KCl_O2-gtensor.out_4cpu
+mpirun -np 4 $gipaw <KCl_O2-hyperfine.in >KCl_O2-hyperfine.out_4cpu
+
+rm -f *.magres
+../../tools/EPR_diagonalize_g-tensor.py KCl_O2-gtensor.out_4cpu >GTENSOR.txt
+grep -A18 spin-densities KCl_O2-hyperfine.out_4cpu >HYPERFINE.txt
